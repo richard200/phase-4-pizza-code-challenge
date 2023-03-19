839ae61792179022f23cb39f5beb25f2f6caf6c7
richard200 <richmn200@gmail.com>
2023-03-19 13:34:13 +0300

    adding routes

--- app/controllers/pitzzas_controller.rb
+++ app/controllers/pitzzas_controller.rb
@@ -1,2 +1,6 @@
 class PitzzasController < ApplicationController
+    def index 
+        pizza = Pitzza.all 
+        render json: pizza
+    end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,15 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def index 
+        rest = Restaurant.all 
+        render json: rest
+    end
+
+
+
+    private
+
+    def render_not_found_response
+      render json: { error: "Restaurant not found" }, status: :not_found
+    end
 end
